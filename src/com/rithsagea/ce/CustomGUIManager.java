30b90e9fc33167f030dca884718832f8960823f1
rithsagea <rithsagea@rithsageas-imac.home>
2018-03-15 17:40:50 -0400

    Removed Everything, Redo This When Knowledge Is Learned

--- src/com/rithsagea/ce/CustomGUIManager.java
+++ src/com/rithsagea/ce/CustomGUIManager.java
@@ -6,14 +6,33 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import com.rithsagea.ce.datatypes.Rarity;
+
 public class CustomGUIManager implements Listener {
+
+	private final static Inventory enchantInventory = Bukkit.createInventory(null, 9, "CustomEnchantments");
 	
-	private final static ItemStack emptySlot = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 8);
-	
-	private final static Inventory enchantInventory = Bukkit.createInventory(null, 45, "CustomEnchantments");
+	@EventHandler
+	public void onEnable(PluginEnableEvent event) {
+		ItemStack commonEnchantIcon = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 8);
+		commonEnchantIcon.getItemMeta().setDisplayName(Rarity.COMMON.getColor() + "Common Enchantment");
+		
+		ItemStack uncommonEnchantIcon = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 5);
+		uncommonEnchantIcon.getItemMeta().setDisplayName(Rarity.UNCOMMON.getColor() + "Uncommon Enchantment");
+		
+		ItemStack rareEnchantIcon = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 4);
+		rareEnchantIcon.getItemMeta().setDisplayName(Rarity.RARE.getColor() + "Rare Enchantment");
+		
+		ItemStack epicEnchantIcon = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 3);
+		epicEnchantIcon.getItemMeta().setDisplayName(Rarity.EPIC.getColor() + "Epic Enchantment");
+		
+		ItemStack legendaryEnchantIcon = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 1);
+		legendaryEnchantIcon.getItemMeta().setDisplayName(Rarity.LEGENDARY.getColor() + "Legendary Enchantment");
+	}
 	
 	@EventHandler
 	public void onInventoryClose(InventoryCloseEvent event) {
